4545b2c916710cbb55089acc37897df8883d0eb5
UNNGU0704 <kyuhung1001@gmail.com>
2024-12-15 18:35:47 +0900

    # 이미지좀 뜨게 해주세요

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1734254042',resources: [
+    cacheName: 'chirpy-1734255335',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

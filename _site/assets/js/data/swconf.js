affebd31659ecf0db024707968f4b800b0753b93
UNNGU0704 <kyuhung1001@gmail.com>
2024-12-15 19:52:35 +0900

    # 이미지 url 재설정

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1734259197',resources: [
+    cacheName: 'chirpy-1734259935',resources: [
       '/learninghub/assets/css/jekyll-theme-chirpy.css',
       '/learninghub/',
       

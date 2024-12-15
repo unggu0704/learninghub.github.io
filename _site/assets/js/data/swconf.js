7a81704e2f384b331d80df05df9844b95e568a93
UNNGU0704 <kyuhung1001@gmail.com>
2024-12-15 18:45:34 +0900

    # 이미지가 보이게 했으면

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1734255335',resources: [
+    cacheName: 'chirpy-1734255919',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

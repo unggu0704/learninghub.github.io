5e45ef78780921bdb46def81dfa99f856f778ad8
UNNGU0704 <kyuhung1001@gmail.com>
2024-12-15 18:14:52 +0900

    # 이미지 경로 재설정

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1733318641',resources: [
+    cacheName: 'chirpy-1734254042',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
